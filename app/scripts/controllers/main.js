c164a85770cad523557f50289de69b2eeb71051f
shunmugaQB <shunmuga@qburst.com>
2016-05-31 18:59:35 +0530

    trip-planner-ui-form-validation

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,29 +8,71 @@
  * Controller of the TripPlanner
  */
 angular.module('TripPlanner')
-  .controller('MainCtrl', function ($scope, $timeout, $q, $log) {
-        var $scope = this;
-   
+  .controller('MainCtrl', function ($scope, $timeout, $q, $log, $http) {
+    var $scope = this;
+    $scope.simulateQuery = false;
+    $scope.isDisabled    = false;
     // list of `state` value/display objects
     $scope.states        = loadAll();
     $scope.querySearch   = querySearch;
     $scope.selectedItemChange = selectedItemChange;
     $scope.searchTextChange   = searchTextChange;
+    $scope.createFilterFor  = createFilterFor;
     $scope.submit = submit;
     $scope.startDate = new Date();
     $scope.EndDate;
     $scope.stop = ""
-    $scope.newState = newState;
+    $scope.addTable = addTable;
+    $scope.delTable = delTable;
+    $scope.checInputFields = checInputFields;
+    $scope.tablesData = [];
 
-    function newState(state) {
-      alert("Sorry! You'll need to create a Constituion for " + state + " first!");
+
+
+     function addTable (index) {
+
+    }
+
+    function checInputFields () {
+     var endDate = Date.parse($scope.EndDate);
+       if($scope.destinationText != null && $scope.originText != null && $scope.stop >0 && !isNaN(endDate))
+        {
+        if($scope.tablesData.length === 0){
+         return false;
+         }
+       return true;
+       }
+
+      return true;
+    }
+
+    function delTable (index) {
     }
 
     function submit () {
-           }
+       var endDate = Date.parse($scope.EndDate)
+       if($scope.destinationText != null && $scope.originText != null && $scope.stop >0 && !isNaN(endDate))
+       {
+        var startYear = $scope.startDate.getFullYear();
+        var startMonth = $scope.startDate.getMonth() + 1;
+        var startDay = $scope.startDate.getDate();
+        var startCalendar = startMonth+'/'+startDay+'/'+startYear;
+        var endYear = $scope.EndDate.getFullYear();
+        var endMonth = $scope.EndDate.getMonth() + 1;
+        var endDay = $scope.EndDate.getDate();
+        var endCalendar = endMonth+'/'+endDay+'/'+endYear;
+
+         var tableData = {"origin":$scope.originText,
+          "destination":$scope.destinationText,
+          "startDate": startCalendar,
+          "endDate": endCalendar}
+
+        $scope.tablesData.push(tableData);
+       }
+    }
 
     function querySearch (query) {
-      var results = query ? $scope.states.filter( createFilterFor(query) ) : $scope.states,
+      var results = query ? $scope.states.filter( $scope.createFilterFor(query) ) : $scope.states,
           deferred;
       if ($scope.simulateQuery) {
         deferred = $q.defer();
